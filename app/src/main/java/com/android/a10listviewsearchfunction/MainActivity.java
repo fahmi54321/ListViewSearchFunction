6aee22949fe2cf91fbd1fc239a14a46e8e5a0b33
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-05-01 16:39:46 +0700

    listview search function

--- app/src/main/java/com/android/a10listviewsearchfunction/MainActivity.java
+++ app/src/main/java/com/android/a10listviewsearchfunction/MainActivity.java
@@ -3,12 +3,47 @@ package com.android.a10listviewsearchfunction;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private EditText search;
+    private ArrayAdapter<String> adapter;
+
+    String data[] = {
+            "fahmi","abdul","aziz","fitria","alya","putri"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listSearch);
+        search = findViewById(R.id.search);
+        adapter = new ArrayAdapter<>(this,R.layout.list_item,R.id.textview,data);
+        listView.setAdapter(adapter);
+
+        search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                MainActivity.this.adapter.getFilter().filter(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
