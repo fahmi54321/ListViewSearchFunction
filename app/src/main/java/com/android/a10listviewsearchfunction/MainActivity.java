81eb2e18c5463e8a714967e90b83bc1038caa517
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-05-01 16:25:35 +0700

    Initial commit

---
+++ app/src/main/java/com/android/a10listviewsearchfunction/MainActivity.java
@@ -0,0 +1,14 @@
+package com.android.a10listviewsearchfunction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
